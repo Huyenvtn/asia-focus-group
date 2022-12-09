6ebfb14bc70ac13d99b8005c1ab247f545302090
huyenvtn <huyenvtn0210@gmail.com>
2022-12-09 16:52:51 +0700

    Add vendor & about page

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,3 +1,101 @@
 export default function About() {
-  return <div>About</div>
+  return(
+    <div>
+      <section id="about-hero" className="d-flex flex-column justify-content-center align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-5 pt-5 pt-lg-0">
+              <h5 data-aos="fade-up">About Us</h5>
+              <br />
+              <h1 data-aos="fade-up">Giving wings<br />to your dreams</h1>
+
+            </div>
+            <div className="col-xl-6 col-lg-5 pt-5 pt-lg-0">
+              <p data-aos="fade-up" data-aos-delay="500">
+                With their experience in legacy businesses, the team of expert serial entrepreneurs recognized the undeniable potential of technology as a medium for disruption across industries.
+                <br /><br />We have expanded our business portfolio to gain a foothold in emerging industries bringing in an ever growing network.
+                <br /><br />Headquartered in the vibrant and bustling financial centre of Singapore, we are at the heart of an ever-changing technology landscape. With its strategic location, we are poised to grow our businesses with a global perspective.
+                <br /><br />Through our partners and strategic networks across Asia and USA, we are able to connect each part of the global economy to our extended group of companies and portfolio. We have created significant economic and social impact beyond the Asian region.
+              </p>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <main id="main">
+
+        <section id="about" className="about">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-xl-6 col-lg-5 pt-5 pt-lg-0">
+                        <h5 data-aos="fade-up">our mission</h5>
+                        <br />
+                        <h1 data-aos="fade-up">Disrupting technology consultancies</h1>
+                        <p data-aos="fade-up">
+                            With a three-pronged purpose as a venture enabler.
+                            <br /><br />With a fundamental belief in the power of people, we will nature talented entrepreneurs from the ground up. Three pillars guide this revolutionary ecosystem:
+                        </p>
+
+                    </div>
+                </div>
+                <div className="row pt-5">
+                    <div className="col-xl-4 col-lg-4 pt-5 pt-lg-0">
+                        <img src="/static/images/icons/shared-community.png" data-aos="fade-up" className="img-fluid afg-icon"></img>
+                        <h4 data-aos="fade-up">Shared Community</h4>
+                        <p data-aos="fade-up">
+                            • Community building and managing initiatives shared across all platforms<br /><br />• Cross-promotion across existing communities<br /><br />• Increased utility of products and services for all communities
+                        </p>
+
+                    </div>
+
+                    <div className="col-xl-4 col-lg-4 pt-5 pt-lg-0">
+                        <img src="/static/images/icons/shared-expertise.png" data-aos="fade-up" className="img-fluid afg-icon"></img>
+                        <h4 data-aos="fade-up">Shared Expertise</h4>
+                        <p data-aos="fade-up">
+                            • Functional and practical expertise leveraged across all platforms<br /><br />• Expanded advisor network<br /><br />• Cost-savings on operational costs of all platforms and apps
+                        </p>
+
+                    </div>
+
+                    <div className="col-xl-4 col-lg-4 pt-5 pt-lg-0">
+                        <img src="/static/images/icons/shared-database.png" data-aos="fade-up" className="img-fluid afg-icon"></img>
+                        <h4 data-aos="fade-up">Shared Database</h4>
+                        <p data-aos="fade-up">
+                            • Reduced growth inhibitors from regulatory pressures<br /><br />• A shared database of customers across all platforms<br /><br />• Ease of access to all services for customers
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="about-mid-hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-xl-6 col-lg-5 pt-5 pt-lg-0">
+                        <h5 data-aos="fade-up">Our Vision</h5>
+                        <br />
+                        <h1 data-aos="fade-up">empowering world-class ventures to create global impact</h1>
+                        <p data-aos="fade-up">
+                            Our hands-on approach guides entrepreneurs from seed to scale through unparalleled mentorship, expertise and support.
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        {/* End Hero */}
+
+
+
+    </main>
+    {/* End #main */}
+    <br />
+    </div>
+  )
 } 
